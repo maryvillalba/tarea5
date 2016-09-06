f6f44a2d94873985302742bcb137742faee4c9b0
Antonia Villalba <maryvillalba994@gmail.com>
2016-09-05 20:07:42 -0400

    tarea5

---
+++ js/app.js
@@ -0,0 +1,20 @@
+(function () {
+  console.log('Hola mundo');
+}());
+var variableArray = [];
+
+var variableString = 'this is String';
+
+var variableNumerico = 69;
+
+variableArray.push('im here');
+variableArray.push('without you'); 
+
+console.log(variableArray);
+
+var elObjeto = {};
+
+elObjeto.descripcion = 'es un objetoo';
+elObjeto.cantidad = 1;
+
+console.log(elObjeto);
